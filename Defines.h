db73d4c76aa3706f56e2aa7d84a9cd562bec54cc
nikolai-hub <110049804+nikolai-hub@users.noreply.github.com>
2022-10-03 20:13:16 +0300

    Add files via upload

---
+++ Defines.h
@@ -0,0 +1,15 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#include <iostream>
+#include <string>
+
+#define  STANDARD_TEMPLATE_LIBRARY  namespace std;
+
+#ifndef DONT_COMPILE_THIS
+#define DONT_COMPILE_THIS
+#endif // !DONT_COMPILE_THIS
+
+
+
+#endif // !DEFINES_H
